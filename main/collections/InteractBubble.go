b74b837ff7d59cb10e1979d5ebe8a6de5974a0c0
ezzhang8 <55414915+ezzhang8@users.noreply.github.com>
2020-10-15 17:59:36 -0700

    october commit
    test

--- main/collections/InteractBubble.go
+++ main/collections/InteractBubble.go
@@ -59,65 +59,9 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "label"
-  type: "label"
-  data: "size {\n"
-  "  x: 25.0\n"
-  "  y: 25.0\n"
-  "  z: 0.0\n"
-  "  w: 0.0\n"
-  "}\n"
-  "scale {\n"
-  "  x: 0.5\n"
-  "  y: 0.5\n"
-  "  z: 1.0\n"
-  "  w: 0.0\n"
-  "}\n"
-  "color {\n"
-  "  x: 1.0\n"
-  "  y: 1.0\n"
-  "  z: 1.0\n"
-  "  w: 1.0\n"
-  "}\n"
-  "outline {\n"
-  "  x: 0.0\n"
-  "  y: 0.0\n"
-  "  z: 0.0\n"
-  "  w: 1.0\n"
-  "}\n"
-  "shadow {\n"
-  "  x: 0.0\n"
-  "  y: 0.0\n"
-  "  z: 0.0\n"
-  "  w: 1.0\n"
-  "}\n"
-  "leading: 1.0\n"
-  "tracking: 0.0\n"
-  "pivot: PIVOT_CENTER\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  "line_break: false\n"
-  "text: \"\\n"
-  "\"\n"
-  "  \"\"\n"
-  "font: \"/assets/Pixel.font\"\n"
-  "material: \"/builtins/fonts/label.material\"\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 1.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/assets/untitled.atlas\"\n"
+  data: "tile_set: \"/assets/Inputs.atlas\"\n"
   "default_animation: \"KeyE\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
